e682b1a3102aa89bfdfcfa310b53169de8211f47
Sunil <sunil.nunubilli@gmail.com>
2024-11-27 23:33:19 +0530

    jenkins

---
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/jenkins-install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#check whether root user or not
+R="\e[31m"
+N="\e[0m"
+
+yum install -y yum-utils
+sudo curl -o /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+# Add required dependencies for the jenkins package
+sudo yum install fontconfig java-17-openjdk
+sudo yum install jenkins
+sudo systemctl daemon-reload
